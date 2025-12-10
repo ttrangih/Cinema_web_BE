ca2e6737a8830f175a48b38561202b061c05ff7e
ttrangih <22521514@gm.uit.edu.vn>
2025-12-10 11:21:25 +0700

    fix login api

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,48 +1,28 @@
 const { query } = require("../config/db");
 
-// Find user by email
-async function findUserByEmail(email) {
+
+//find user by email
+async function findUserById(id) {
   const result = await query(
     `
     SELECT
-      id          AS "Id",
-      fullname    AS "FullName",
-      email       AS "Email",
+      id           AS "Id",
+      fullname     AS "FullName",
+      email        AS "Email",
       passwordhash AS "PasswordHash",
-      role        AS "Role",
-      createdat   AS "CreatedAt"
+      role         AS "Role",
+      createdat    AS "CreatedAt"
     FROM users
-    WHERE email = $1
-  `,
-    [email]
-  );
-
-  return result.rows[0]; // không có thì = undefined
-
-}
-
-// Create a new user
-async function createUser({ fullName, email, passwordHash }) {
-  const result = await query(
-    `
-    INSERT INTO users (fullname, email, passwordhash)
-    VALUES ($1, $2, $3)
-    RETURNING
-      id          AS "Id",
-      fullname    AS "FullName",
-      email       AS "Email",
-      passwordhash AS "PasswordHash",
-      role        AS "Role",
-      createdat   AS "CreatedAt"
-  `,
-    [fullName, email, passwordHash]
+    WHERE id = $1
+    `,
+    [id]
   );
 
   return result.rows[0];
-
 }
 
 module.exports = {
   findUserByEmail,
   createUser,
+  findUserById,   
 };
